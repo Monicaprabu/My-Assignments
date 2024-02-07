46035257d2065869b9e44121ee3b0e0f49f85386
Monicaprabu <159238178+Monicaprabu@users.noreply.github.com>
2024-02-07 13:39:21 +0530

    Add files via upload

---
+++ day2/LearnMethodandObjects.java
@@ -0,0 +1,32 @@
+package week1.day2;
+
+
+
+public class LearnMethodandObjects {
+
+	public String launchbrowser(String browsername)
+	{
+		return browsername;
+	}
+public void loadURL() 
+{
+	System.out.println("URL loaded successfully");
+	
+}
+public static void main(String[] args) {
+	LearnMethodandObjects a=new LearnMethodandObjects ();
+System.out.println(a.launchbrowser("browser launch successfully"));
+a.loadURL();
+	
+
+
+	
+	
+}
+	
+} 
+	
+
+	
+
+
---
+++ day2/PrintOddNumber.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class PrintOddNumber {
+
+	public static void main(String[] args) {
+		int odd=25;
+		for (int i = 0; i <=odd; i++) {
+			if(i%2==1) {
+				System.out.println("Odd number is :" +i);
+			}
+		}
+
+	}
+
+}
